a562db8a62e5dc59905209c327e098cb12756128
skyfalko-coding <hi.falko@gmail.com>
2024-11-06 19:48:07 +0100

    aufgabe 1b, c

--- Uebung/ex01.hs
+++ Uebung/ex01.hs
@@ -9,8 +9,32 @@
 
 
 -- Zusammengefasst: 
---     (lehmann ⇒ ¬mueller)∧(mueller ⇒ ¬Schulze)∧(schulze ⇒ (¬lehmann ∧ ¬mueller))
--- ⇔  (lehmann ⇒ ¬mueller)∧(mueller ⇒ ¬Schulze)∧(schulze ⇒ ¬(lehmann v mueller))
+--     (lehmann ⇒ ¬mueller)∧(mueller ⇒ ¬schulze)∧(schulze ⇒ (¬lehmann ∧ ¬mueller))
+-- ⇔  (lehmann ⇒ ¬mueller)∧(mueller ⇒ ¬schulze)∧(schulze ⇒ ¬(lehmann v mueller))
 -- ⇔  (¬lehmann v  mueller)∧(¬mueller v Schulze)∧(¬schulze v (lehmann v mueller))
 
-b
+-- b)
+
+-- l := lehmann
+-- m := mkDuplexHandleNoFinalizer
+-- s := schulze
+
+-- l   m   s   (¬l v  m)   (¬m v s)    (¬s v (l v m))    (¬l v  m)∧(¬m v s)∧(¬s v (l v m))
+-- 0   0   0       1           1               1                       1
+-- 0   0   1       1           1               0                       0
+-- 0   1   0       1           0               1                       0
+-- 0   1   1       1           1               1                       1
+-- 1   0   0       0           1               1                       0
+-- 1   0   1       0           1               1                       0
+-- 1   1   0       1           0               1                       0
+-- 1   1   1       1           1               1                       1
+
+-- Nur wahre Wahrheitswerte für:
+
+--     -  Lehmann lügt, Müller lügt, Schulze lügt
+--     -  Lehmann lügt, Müller sagt die Wahrheit, Schulze sagt die Wahrheit
+--     -  Lehmann sagt die Wahrheit,  Müller sagt die Wahrheit, Schulze sagt die Wahrheit
+
+
+riddle :: Bool -> Bool -> Bool -> Bool
+riddle l m s = (not l ||  m)&&(not m || s)&&(not s || (l || m))
